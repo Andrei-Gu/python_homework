9719b73b6f1c6134f7c728f6d3b501e2e38087d7
Andrei-Gu <94022593+Andrei-Gu@users.noreply.github.com>
2023-04-12 05:40:26 +0500

    Delete homework_08 directory

--- homework_08/mainapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import InstrumentalResearchCategory, InstrumentalResearch, Preparation, InstrumentalResearchCard
-
-
-admin.site.register(InstrumentalResearchCategory)
-admin.site.register(InstrumentalResearch)
-admin.site.register(Preparation)
-admin.site.register(InstrumentalResearchCard)
